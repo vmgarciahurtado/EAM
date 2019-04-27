1f92968fdb5468aff8a6f0ef888638bf83c79bb2
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-04-26 20:41:19 -0500

    Se hicieron modificaciones

--- app/src/main/java/com/example/victor/eam/estudiante/PrincipalEstudiante.java
+++ app/src/main/java/com/example/victor/eam/estudiante/PrincipalEstudiante.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.victor.eam.R;
+
 public class PrincipalEstudiante extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
