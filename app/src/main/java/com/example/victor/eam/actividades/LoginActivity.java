40dd0279bd6682ecb3292a75a80402ebe9cd6526
vmgarciahurtado <vmgarciahurtado@gmail.com>
2019-04-28 20:17:48 -0500

    Se hicieron modificaciones

--- app/src/main/java/com/example/victor/eam/actividades/LoginActivity.java
+++ app/src/main/java/com/example/victor/eam/actividades/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.victor.eam.R;
+import com.example.victor.eam.director_programa.PrincipalDirectorPrograma;
 import com.example.victor.eam.estudiante.PrincipalEstudiante;
 import com.example.victor.eam.registro_control.PrincipalRegistroControl;
 
@@ -17,8 +18,23 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void ventana(View view) {
+    public void estudiante(View view) {
         Intent intent = new Intent(getApplicationContext(),PrincipalEstudiante.class);
         startActivity(intent);
     }
+
+    public void registroControl(View view) {
+        Intent intent = new Intent(getApplicationContext(),PrincipalRegistroControl.class);
+        startActivity(intent);
+    }
+
+    public void directorPrograma(View view) {
+        Intent intent = new Intent(getApplicationContext(),PrincipalDirectorPrograma.class);
+        startActivity(intent);
+    }
+
+   /* public void docente(View view) {
+        Intent intent = new Intent(getApplicationContext(),Principal.class);
+        startActivity(intent);
+    }*/
 }
---
+++ app/src/main/java/com/example/victor/eam/director_programa/PrincipalDirectorPrograma.java
@@ -0,0 +1,93 @@
+package com.example.victor.eam.director_programa;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.victor.eam.R;
+import com.example.victor.eam.entidades.AllFragments;
+import com.example.victor.eam.entidades.PrincipalPantallas;
+
+public class PrincipalDirectorPrograma extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,AllFragments {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_director_programa);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Este es el fragmet que se carga de primero
+        Fragment miFragment = new PrincipalPantallas();
+        getSupportFragmentManager().beginTransaction().replace(R.id.containerDirectorPrograma, miFragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            //Este es el fragmet que se carga de primero
+            Fragment miFragment = new PrincipalPantallas();
+            getSupportFragmentManager().beginTransaction().replace(R.id.containerDirectorPrograma, miFragment).commit();
+
+        } else if (id == R.id.nav_gallery) {
+            //Este es el fragmet que se carga de primero
+            Fragment miFragment = new AsignarMaterias();
+            getSupportFragmentManager().beginTransaction().replace(R.id.containerDirectorPrograma, miFragment).commit();
+
+        } else if (id == R.id.nav_slideshow) {
+            //Este es el fragmet que se carga de primero
+            Fragment miFragment = new CreacionCursos();
+            getSupportFragmentManager().beginTransaction().replace(R.id.containerDirectorPrograma, miFragment).commit();
+        } else if (id == R.id.nav_share) {
+            //Cerrar sesion
+        } else if (id == R.id.nav_send) {
+            //Cerrar la aplicacion
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
+    }
+}
