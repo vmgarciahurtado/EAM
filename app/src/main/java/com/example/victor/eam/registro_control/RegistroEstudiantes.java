46087e380ff8eb04997d5326bb9f53814e613511
JU4NG1R4L <juanj8845@gmail.com>
2019-05-10 20:13:06 -0500

    Se realizaron cambios

--- app/src/main/java/com/example/victor/eam/registro_control/RegistroEstudiantes.java
+++ app/src/main/java/com/example/victor/eam/registro_control/RegistroEstudiantes.java
@@ -19,6 +19,7 @@ import com.example.victor.eam.R;
  * create an instance of this fragment.
  */
 public class RegistroEstudiantes extends Fragment {
+//Juanfer la mama
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
